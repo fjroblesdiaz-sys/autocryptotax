380036452621092e7ad15ca6b30dc6dff5892c94
mibald <miguelaless.dev@gmail.com>
2025-11-10 16:26:38 -0400

    Add support for multiple Web3 wallets and social login in ConnectWalletButton component

--- src/features/home/components/connect-wallet-button.component.tsx
+++ src/features/home/components/connect-wallet-button.component.tsx
@@ -3,15 +3,38 @@
 /**
  * Connect Wallet Button Component
  * Custom wrapper for Thirdweb ConnectButton with custom styling
+ * Includes support for Web3 wallets and social login
  */
 
 import { ConnectButton } from 'thirdweb/react';
+import { createWallet, inAppWallet } from 'thirdweb/wallets';
 import { client } from '@/lib/thirdweb-client';
+
+const wallets = [
+  inAppWallet({
+    auth: {
+      options: [
+        'google',
+        'apple',
+        'facebook',
+        'email',
+        'phone',
+      ],
+    },
+  }),
+  createWallet("io.metamask"),
+  createWallet("com.coinbase.wallet"),
+  createWallet("me.rainbow"),
+  createWallet("io.rabby"),
+  createWallet("app.phantom"),
+  createWallet("walletConnect"),
+];
 
 export function ConnectWalletButton() {
   return (
     <ConnectButton 
-      client={client} 
+      client={client}
+      wallets={wallets}
       theme="dark"
       locale='es_ES'
       connectButton={{
